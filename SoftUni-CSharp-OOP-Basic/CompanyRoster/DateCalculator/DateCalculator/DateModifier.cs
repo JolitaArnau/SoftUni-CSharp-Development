9ab4e5c4b1aa2612e4ea706bef77cb866efa4190
Jolita Arnaudova <jolita.arnaudova@gmail.com>
2018-02-13 18:45:58 +0100

    solved first 6 assignments on the first topic Defining Classes

---
+++ SoftUni-CSharp-OOP-Basic/CompanyRoster/DateCalculator/DateCalculator/DateModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class DateModifier
+{
+    public static void DateDifference(string firstDate, string secondDate)
+    {
+        var d1 = firstDate.Split(new char[0], StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        var d2 = secondDate.Split(new char[0], StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        DateTime first = new DateTime(d1[0], d1[1], d1[2]);
+        DateTime second = new DateTime(d2[0], d2[1], d2[2]);
+
+        TimeSpan difference = first.Subtract(second);
+        Console.WriteLine(Math.Abs(difference.TotalDays));
+    }
+}
---
+++ SoftUni-CSharp-OOP-Basic/CompanyRoster/DateCalculator/DateCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string date1 = Console.ReadLine();
+        string date2 = Console.ReadLine();
+
+
+        DateModifier.DateDifference(date1, date2);
+    }
+}
