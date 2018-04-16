8dde20c86e35acab57bc5d5e6102592fe4294d28
Jolita Arnaudova <jolita.arnaudova@gmail.com>
2018-04-16 12:27:02 +0200

    Personal, not guided by anyone OOP Project, written while in the OOP Advanced Course @SoftUni

---
+++ Simple Finance Manager/Models/Commands/AddCommands/AddCategoryCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+
+public class AddCategoryCommand : Command
+{
+    public AddCategoryCommand(IList<string> arguments, IManager manager) : base(arguments, manager)
+    {
+    }
+
+    public override string Execute() => base.Manager.AddCategory(this.Arguments);
+}
