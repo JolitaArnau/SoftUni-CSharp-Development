c5e3bda45d970ff1fa5fea07125065a71a9f9e0a
Jolita Arnaudova <jolita.arnaudova@gmail.com>
2018-07-13 10:16:11 +0200

    edited connection string

--- SoftUni-Entity-Framework-Core/Advanced Relations/P01_BillsPaymentSystem.Application/Data/Configuration.cs
+++ SoftUni-Entity-Framework-Core/Advanced Relations/P01_BillsPaymentSystem.Application/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public const string ConnectionString = "Server=127.0.0.1,1433;Database=BillsPaymentSystem;User Id=SA;Password=4e6mi4ka!";
+        public const string ConnectionString = "Server=127.0.0.1,1433;Database=BillsPaymentSystem;Integrated Security=True;";
     }
 }
